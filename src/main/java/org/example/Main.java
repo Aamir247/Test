e19c1717f7635507b1420f49070551106a812741
Aamir247 <97893217+Aamir247@users.noreply.github.com>
2025-10-29 18:02:13 +0530

    Updated
    Updated the MapperScan annotation to point to the correct package.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,11 @@
 package org.example;
+
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 @SpringBootApplication
-@MapperScan("org.sample.member.repositories")
+@MapperScan("org.example.member.repositories")  // Fix this line
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
